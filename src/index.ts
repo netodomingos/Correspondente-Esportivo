237d8e2e7933be93001d03f9de9326fac5139942
Geraldo Domingos <geraldo.dsn2000@gmail.com>
2024-04-28 19:20:57 -0300

    ✨feature: adicionando agendamento no projeto

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,24 @@
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
 import schedule from 'node-schedule';
-
 import { handleGetInLiveMatchsInformation } from './modules/GettingLiveMatches';
 import { handleFormatMatchesToMessage } from './modules/FormatMatchesToMessage';
 import { discordMessage } from './modules/DiscordMessage';
 
 dotenv.config();
 
-async function sportsFactory(){
-	const matchesInfo = await handleGetInLiveMatchsInformation()	
-	const matchesFormated = await handleFormatMatchesToMessage(matchesInfo)
-	discordMessage(matchesFormated)
+async function sportsFactory() {
+    try {
+        const matchesInfo = await handleGetInLiveMatchsInformation();	
+        const matchesFormated = await handleFormatMatchesToMessage(matchesInfo);
+        discordMessage(matchesFormated);
+        console.log('Sports update sent successfully.'); // Logging for verification
+    } catch (error) {
+        console.error('Error occurred while sending sports update:', error); // Logging error
+    }
 }
 
-const rules = new schedule.RecurrenceRule();
-rules.hour = 8; 
-rules.minute = 0; 
+const rule = new schedule.RecurrenceRule();
+rule.hour = 8;
+rule.minute = 0;
 
-schedule.scheduleJob(rules, sportsFactory);
+schedule.scheduleJob(rule, sportsFactory);
